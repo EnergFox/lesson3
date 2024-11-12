0c782586dd79a012598ed68bb1ba04a683979ed2
Олег <48029194+EnergFox@users.noreply.github.com>
2024-11-12 23:57:01 +0500

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,5 @@
+completedHw = '12'
+TimeSpent = '1.5'
+CourseName = 'Python'
+TimeForOneTask = '0.125'
+print('Курс:' + CourseName + ',', 'всего задач:' + completedHw + ',', 'затраченно часов:' + TimeSpent + ",", 'среднее время выполнения', TimeForOneTask, 'часа.' )
