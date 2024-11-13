417bee7c34057a34872b686cff1ea9ac1f209e1f
Олег <48029194+EnergFox@users.noreply.github.com>
2024-11-13 23:12:04 +0500

    Updatelesson3.py

--- lesson3.py
+++ lesson3.py
@@ -1,5 +1,6 @@
-completedHw = '12'
-TimeSpent = '1.5'
+completedHw = 12
+TimeSpent = 1.5
 CourseName = 'Python'
-TimeForOneTask = '0.125'
-print('Курс:' + CourseName + ',', 'всего задач:' + completedHw + ',', 'затраченно часов:' + TimeSpent + ",", 'среднее время выполнения', TimeForOneTask, 'часа.' )
+print(TimeSpent / completedHw)
+TimeForOneTask = 0.125
+print('Курс:' + CourseName + ',', 'всего задач:' + (str(completedHw)) + ',', 'затраченно часов:' + (str(TimeSpent)) + ",", 'среднее время выполнения', (str(TimeForOneTask)), 'часа.' )
